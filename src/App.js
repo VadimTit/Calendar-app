3605bea2e60f67400f08c087fc3cc988fa89c0f7
VadimTit <ozzymafiozzii@gmail.com>
2020-09-06 21:22:04 +0300

    start of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Cars from './components/cars';
+
+
+
+const arr = [
+  1, 5, 10
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      Top Cars 
+      <Cars></Cars>
       </header>
     </div>
   );
