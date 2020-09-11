c39df3467a0485f107c9c5c8694ad7e57e6c175e
VadimTit <ozzymafiozzii@gmail.com>
2020-09-11 13:42:12 +0300

    adds days of month

--- src/components/month/month.component.js
+++ src/components/month/month.component.js
@@ -6,10 +6,30 @@ export default class MonthComponent extends Component {
   render() {
     const startDate = this.props.startDate;
     const title = startDate.toLocaleString('default', { month: 'long' });
+    const tempDate = new Date(startDate);
+    const days = [];
+
+    let day = 1;
+    while (tempDate.getMonth() === startDate.getMonth()) {
+      days.push(day);
+      day = day + 1;
+      tempDate.setDate(day);
+    }
+
+    for (let i = startDate.getDay(); i > 0; i--) {
+      days.unshift('');
+    }
+
+    const dayOfWeek = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+
     return (
       <div className="month">
         <div className="month-header">
           {title}
+        </div>
+        <div className="month-main">
+          {dayOfWeek.map(day => <div className="day day-title">{day}</div>)}
+          {days.map(day => <div className="day">{day}</div>)}
         </div>
       </div>
     );
