1abaa827c618672c9c4ad4f9768bfef7c50323ef
VadimTit <ozzymafiozzii@gmail.com>
2020-09-17 08:24:57 +0300

    adds Form

--- src/components/day/day.component.js
+++ src/components/day/day.component.js
@@ -6,9 +6,15 @@ import { Link } from 'react-router-dom';
 export default class DayComponent extends Component {
   render() {
     const date = this.props.date;
+    if (!date) {
+      return (<div className="day"> </div>)
+    }
     const isToday = date && (new Date().toDateString() === date.toDateString());
+    
+    
+    
     return (
-      <Link to="" className="day">
+      <Link to={`/year/${date.getFullYear()}/month/${date.getMonth()}/day/${date.getDate()}`} className="day">
         <div className={classnames({ today: isToday })}>
           {date ? date.getDate() : null}
         </div>
--- src/components/dayPage/dayPage.component.js
+++ src/components/dayPage/dayPage.component.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
+import './dayPage.component.css'
 
 export default class DayPageComponent extends Component {
+  state = {
+    title: 'sample',
+    description: 'sample description',
+  }
+
+  handleTitleChange = (event) => {
+    this.setState({ title: event.target.value });
+  }
+
+  handleDescriptionChange = (event) => {
+    this.setState({ description: event.target.value });
+  }
+  /**
+   * 
+   * @param {import('react').SyntheticEvent} event 
+   */
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <div>
-        <input type="text">
-          
-        </input>
+      <div className="day-page">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleTitleChange}
+          />
+          <input type="text"
+            name="description"
+            value={this.state.description}
+            onChange={this.handleDescriptionChange}
+          />
+          <input type="submit" value="Отправить" />
+        </form>
       </div>
     )
   }
